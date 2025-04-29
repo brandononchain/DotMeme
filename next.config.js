148a3e2cb73894fce49dd331f063a0813cd2ae5b
Brandon M. Crenshaw <109445618+brandononchain@users.noreply.github.com>
2025-04-28 22:38:27 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'], // For DALL-E images
+  },
+}
+
+module.exports = nextConfig
